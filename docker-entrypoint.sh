8247744b8ba16f1a6b29244d61d31dbcb61f23e1
Ahmed-Abdelkhalek <41132805+ahmedabdelkhalek1@users.noreply.github.com>
2025-10-17 23:47:50 +0400

    Refactor tinyproxy configuration handling

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env sh
 set -eu
 
-TEMPLATE="/etc/tinyproxy/tinyproxy.conf"
-OUT="/tmp/tinyproxy.conf"
-
+CONF="/etc/tinyproxy/tinyproxy.conf"
 PORT_VALUE="${PORT:-10000}"
 USER_VALUE="${TINYPROXY_USER:-user}"
 PASS_VALUE="${TINYPROXY_PASS:-pass}"
 
-sed -e "s/__PORT__/${PORT_VALUE}/" \
-    -e "s/__USER__/${USER_VALUE}/" \
-    -e "s/__PASS__/${PASS_VALUE}/" \
-    "$TEMPLATE" > "$OUT"
+sed -i "s/__PORT__/${PORT_VALUE}/" "$CONF"
+sed -i "s/__USER__/${USER_VALUE}/" "$CONF"
+sed -i "s/__PASS__/${PASS_VALUE}/" "$CONF"
 
-exec /opt/tinyproxy/sbin/tinyproxy -d -c "$OUT"
+# drop privileges to the tinyproxy user for the server process
+exec su-exec tinyproxy /opt/tinyproxy/sbin/tinyproxy -d -c "$CONF"
