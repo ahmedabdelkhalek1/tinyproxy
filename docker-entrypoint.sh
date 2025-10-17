b649abd169fc9e222f6034a16a70bab63ae37fd1
Ahmed-Abdelkhalek <41132805+ahmedabdelkhalek1@users.noreply.github.com>
2025-10-17 23:46:24 +0400

    Modify entrypoint script to use output config file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env sh
 set -eu
 
-CONF="/etc/tinyproxy/tinyproxy.conf"
+TEMPLATE="/etc/tinyproxy/tinyproxy.conf"
+OUT="/tmp/tinyproxy.conf"
+
 PORT_VALUE="${PORT:-10000}"
 USER_VALUE="${TINYPROXY_USER:-user}"
 PASS_VALUE="${TINYPROXY_PASS:-pass}"
 
-# Substitute placeholders with env values expected by Render and your secrets
-sed -i "s/__PORT__/${PORT_VALUE}/" "$CONF"
-sed -i "s/__USER__/${USER_VALUE}/" "$CONF"
-sed -i "s/__PASS__/${PASS_VALUE}/" "$CONF"
+sed -e "s/__PORT__/${PORT_VALUE}/" \
+    -e "s/__USER__/${USER_VALUE}/" \
+    -e "s/__PASS__/${PASS_VALUE}/" \
+    "$TEMPLATE" > "$OUT"
 
-# Exec Tinyproxy in foreground so Render can manage the process
-exec /opt/tinyproxy/sbin/tinyproxy -d -c "$CONF"
+exec /opt/tinyproxy/sbin/tinyproxy -d -c "$OUT"
